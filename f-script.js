e8614c0fb086087f11e809abdbe1cb3ea24be40b
Fionn-Orourke <G00429572@atu.ie>
2023-12-12 21:09:59 +0000

    html & java checkout added with more styling

--- f-script.js
+++ f-script.js
@@ -14,3 +14,19 @@ function ImageBlack() {
     var image = document.getElementById('bose3');
     image.src = "fimage/bose3 black.png";
 }
+function checkout(){
+    var b1 = document.getElementById('b1').value;
+    var b2 = document.getElementById('b2').value;
+    var b3 = document.getElementById('b3').value;
+    var tot = document.getElementById('cost');
+    if(b1 < 0 || b2 < 0 || b3 < 0 ){
+        tot.innerText = "Oi dont be negative";
+    }else{
+        var cost = 290* b1+249* b2+244.95* b3;
+        tot.innerText = "Your total cost is €" + cost;
+    }
+    
+
+
+
+}
